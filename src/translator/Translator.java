fa5d4a6e411be95d0e5937377cf1bf4cfd3e95c4
Mark Sverdlov <marksve039@gmail.com>
2021-05-15 18:33:35 +0300

    added a digit enum to handle digits and added in-code integer - digit dictionary.

--- src/translator/Translator.java
+++ src/translator/Translator.java
@@ -4,17 +4,61 @@ public class Translator {
 	private static final int DECIMAL = 10;
 
 	private enum Digit {
-		One, Two, Three, Four, Five, Six, Seven, Eight, Nine;
-	}
+		One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Zero;
 
-	private Digit toDigit(int digit) {
-		switch (digit) {
-		case 1:
-			return Digit.One;
-		case 2: return Digit.Two;
-		case 3: return Digit.Three;
-		case 4: return Digit.Four;
-		
+		int toNumber() {
+			switch (this) {
+			case Eight:
+				return 8;
+			case Five:
+				return 5;
+			case Four:
+				return 4;
+			case Nine:
+				return 9;
+			case One:
+				return 1;
+			case Seven:
+				return 7;
+			case Six:
+				return 6;
+			case Three:
+				return 3;
+			case Two:
+				return 2;
+			case Zero:
+				return 0;
+			default:
+				return -1; // this shouldn't happen
+
+			}
+		}
+
+		static Digit toDigit(int digit) {
+			switch (digit) {
+			case 1:
+				return Digit.One;
+			case 2:
+				return Digit.Two;
+			case 3:
+				return Digit.Three;
+			case 4:
+				return Digit.Four;
+			case 5:
+				return Digit.Five;
+			case 6:
+				return Digit.Six;
+			case 7:
+				return Digit.Seven;
+			case 8:
+				return Digit.Eight;
+			case 9:
+				return Digit.Nine;
+			case 0:
+				return Digit.Zero;
+			default:
+				throw new IllegalArgumentException("Number: " + digit + " is not a digit.");
+			}
 		}
 	}
 
@@ -31,6 +75,6 @@ public class Translator {
 			number /= DECIMAL;
 		}
 
-		return number % DECIMAL;
+		return Digit.toDigit(n % DECIMAL);
 	}
 }
